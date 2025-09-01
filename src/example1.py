21355a2a967dc377d94b5d1cceffe3c57599dd89
abp3 <abp3@sfu.ca>
2025-09-01 15:57:34 -0700

    test

---
+++ src/example1.py
@@ -0,0 +1,7 @@
+import os
+import sys  # unused import
+import math
+
+def greet(name):
+    print("Hello " + name)   # missing space before '+'
+    x = 10  # unused variable
---
+++ src/example2.py
@@ -0,0 +1,4 @@
+def add(a,b):  # missing space after comma
+    return a+b
+
+unused_var = 100
